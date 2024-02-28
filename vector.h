f18cc4496692b32262b11670e3088e18d100c709
davinianunez <159300167+davinianunez@users.noreply.github.com>
2024-02-28 13:56:28 -0400

    Create vector.h

---
+++ vector.h
@@ -0,0 +1,16 @@
+class Vector
+{
+private:
+	int vec[10], n;
+public:
+	Vector(void); //constructor
+	~Vector(void); //destructor
+	void set_n(int _n);
+	int get_n();
+	void cargarVector(int vec[], int n); //metodos
+	void mostrarVector(int vec[], int n);
+	void ordenarVector(int vec[], int n);
+};
+
+
+
